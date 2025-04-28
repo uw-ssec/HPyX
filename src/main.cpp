3be40ac85eebe13f9b71e38b8c8d272d3d0f55ea
Don Setiawan <landungs@uw.edu>
2025-04-28 14:22:33 -0700

    feat: Add codespaces with pixi config and simple hpx_hello test (#4)
    * build: Update dependencies to get the build to work
    
    * chore: Add devcontainer workspace
    
    * chore: Add hostRequirements to devcontainer
    
    * chore: Update home setup
    
    * chore(deps): Update dependencies, try out hpx_hello
    
    * style: pre-commit fixes
    
    * build: Fix package name to lowercase
    
    * chore: Update cmakelists to use skbuild project name at the end
    
    * ci: Remove windows build for now
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <nanobind/nanobind.h>
+#include <hpx/hpx_main.hpp>
+#include <hpx/iostream.hpp>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -6,9 +8,17 @@
 namespace nb = nanobind;
 using namespace nb::literals;
 
+int hpx_hello()
+{
+    // Say hello to the world!
+    hpx::cout << "Hello World!\n" << std::flush;
+    return 0;
+}
+
 NB_MODULE(_core, m) {
     m.doc() = "Python bindings for HPX C++ API";
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+    m.def("hpx_hello", &hpx_hello);
 
     #ifdef VERSION_INFO
         m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
