2234ee29de4f43bc1bf3317e3bdca5471cd38b41
Madeline <madgord@uw.edu>
2025-07-08 16:23:58 -0700

    build: linux build changes for thread safety

--- tests/test_hpx_linalg.py
+++ tests/test_hpx_linalg.py
@@ -4,7 +4,7 @@ import pytest
 import hpyx
 from hpyx.runtime import HPXRuntime
 
-@pytest.mark.parametrize("v_len", [10e1, 10e2, 10e3, 10e4, 10e5, 10e6])
+@pytest.mark.parametrize("v_len", [int(x) for x in [10e1, 10e2, 10e3, 10e4, 10e5, 10e6]])
 def test_hpx_dot1d(v_len):
     rng = np.random.default_rng()
     a = rng.random(int(v_len))
