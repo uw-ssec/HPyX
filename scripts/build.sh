8598c9c0cdb793684df89048a9244d95d22b6509
Landung 'Don' Setiawan <landungs@uw.edu>
2025-07-16 09:49:35 -0700

    build: Update hpx dependency to build from source
    Updated the default hpx dependency to build
    from source rather than conda. This allows
    for more flexibility of build configuration
    for hpx.
    
    ---
    
    Related Issue: https://github.com/uw-ssec/HPyX/issues/47

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,60 @@
 # See https://github.com/conda-forge/hpx-feedstock/blob/main/recipe/build.sh
 set -e
 
+# Set default values
+MALLOC="system"
 BUILD_DIR="build"
+HPX_VERSION=""
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --malloc)
+      MALLOC="$2"
+      shift 2
+      ;;
+    --build-dir)
+      BUILD_DIR="$2"
+      shift 2
+      ;;
+    --hpx-version)
+      HPX_VERSION="$2"
+      shift 2
+      ;;
+    -h|--help)
+      echo "Usage: $0 [--malloc TYPE] [--build-dir DIR] --hpx-version VERSION"
+      echo "  --malloc TYPE        Set malloc implementation (default: system)"
+      echo "  --build-dir DIR      Set build directory (default: build)"
+      echo "  --hpx-version VERSION Set HPX version to build from source (required)"
+      echo "  -h, --help           Show this help message"
+      exit 0
+      ;;
+    *)
+      echo "Unknown option $1"
+      echo "Usage: $0 [--malloc TYPE] [--build-dir DIR] --hpx-version VERSION"
+      echo "Use --help for more information"
+      exit 1
+      ;;
+  esac
+done
+
+# Check if HPX_VERSION is required and provided
+if [ -z "$HPX_VERSION" ]; then
+  echo "Error: HPX version must be specified with --hpx-version"
+  echo "Usage: $0 [--malloc TYPE] [--build-dir DIR] --hpx-version VERSION"
+  exit 1
+fi
+
+if [ -n "$HPX_VERSION" ]; then
+  echo "Building HPX Version: $HPX_VERSION"
+fi
+echo "Building with memory allocator: $MALLOC"
+echo "Using build directory: $BUILD_DIR"
+
+git checkout "$HPX_VERSION" || {
+  echo "Error: HPX version $HPX_VERSION not found in the repository."
+  exit 1
+}
 
 if [ -d "$BUILD_DIR" ]; then
   echo "$BUILD_DIR exists."
@@ -12,7 +65,7 @@ fi
 
 if [ ! -d "$BUILD_DIR" ]; then
   echo "$BUILD_DIR does not exist."
-  mkdir build
+  mkdir "$BUILD_DIR"
 fi
 
 pushd $BUILD_DIR
@@ -29,7 +82,7 @@ cmake \
     -D CMAKE_INSTALL_LIBDIR=lib \
     -D PYTHON_EXECUTABLE="$PYTHON" \
     -D HPX_WITH_EXAMPLES=FALSE \
-    -D HPX_WITH_MALLOC="tcmalloc" \
+    -D HPX_WITH_MALLOC="$MALLOC" \
     -D HPX_WITH_NETWORKING=FALSE \
     -D HPX_WITH_TESTS=FALSE \
     ..
