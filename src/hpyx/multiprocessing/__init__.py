10d32c651290d1def05bf96cc9c542281a6588a6
Landung 'Don' Setiawan <landungs@uw.edu>
2025-08-11 14:15:31 -0700

    chore: Refine module listings for documentation sake

--- src/hpyx/multiprocessing/__init__.py
+++ src/hpyx/multiprocessing/__init__.py
@@ -3,6 +3,6 @@ HPyX Multiprocessing subpackage.
 This subpackage provides support for multiprocessing in HPyX.
 """
 from __future__ import annotations
-from .for_loop import for_loop
+from ._for_loop import for_loop
 
 __all__ = ["for_loop"]
